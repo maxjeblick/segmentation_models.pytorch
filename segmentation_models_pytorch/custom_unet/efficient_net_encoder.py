570fa3173cdad76c92b71bc6ab4feef77a8b9bac
max <sk8maxe@web.de>
2019-09-03 16:24:08 +0200

    added efficientnet encoder

--- segmentation_models_pytorch/custom_unet/efficient_net_encoder.py
+++ segmentation_models_pytorch/custom_unet/efficient_net_encoder.py
@@ -5,6 +5,11 @@ from efficientnet_pytorch.utils import load_pretrained_weights, get_model_params
 
 class EfficientNetEncoder(EfficientNet):
 
+    def __init__(self, model_name='efficientnet-b0'):
+        blocks_args, global_params = get_model_params(model_name, override_params={})
+        super().__init__(blocks_args, global_params)
+        load_pretrained_weights(self, model_name=model_name, load_fc=True)
+
     def extract_features(self, inputs):
         """ Returns output of the final convolution layer """
 
@@ -28,16 +33,31 @@ class EfficientNetEncoder(EfficientNet):
         """ Calls extract_features to extract features, applies final linear layer, and returns logits. """
 
         # Convolution layers
-        return self.extract_features(inputs)
+        features = self.extract_features(inputs)
+        idxs = [0, 2, 4, 10, -1]
+        return [features[idx] for idx in idxs][::-1]
 
 
 if __name__ == '__main__':
-    model_name = 'efficientnet-b0'
+    model = EfficientNetEncoder()
 
-    blocks_args, global_params = get_model_params(model_name, override_params={})
-
-    model = EfficientNetEncoder(blocks_args, global_params)
-    load_pretrained_weights(model, model_name=model_name, load_fc=True)
-
-    x = torch.rand((11, 3, 128, 128))
+    x = torch.rand((11, 3, 256, 256))
     print([block.shape for block in model(x)])
+"""
+0: torch.Size([11, 16, 128, 128]),  <-
+torch.Size([11, 24, 64, 64]),
+2: torch.Size([11, 24, 64, 64]),
+torch.Size([11, 40, 32, 32]), 
+4: torch.Size([11, 40, 32, 32]),   <-
+torch.Size([11, 80, 16, 16]), 
+torch.Size([11, 80, 16, 16]), 
+torch.Size([11, 80, 16, 16]), 
+torch.Size([11, 112, 16, 16]), 
+torch.Size([11, 112, 16, 16]), 
+10: torch.Size([11, 112, 16, 16]),   <-
+torch.Size([11, 192, 8, 8]), 
+torch.Size([11, 192, 8, 8]), 
+torch.Size([11, 192, 8, 8]), 
+torch.Size([11, 192, 8, 8]), 
+-1: torch.Size([11, 320, 8, 8])   <-
+"""
