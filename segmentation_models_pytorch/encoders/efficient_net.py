bde4236a4b8396d72c99a606fa2eee3a3dddb390
max <sk8maxe@web.de>
2019-09-04 17:15:27 +0200

    added efficientnet b1,b2, and b3

--- segmentation_models_pytorch/encoders/efficient_net.py
+++ segmentation_models_pytorch/encoders/efficient_net.py
@@ -34,22 +34,20 @@ class EfficientNetEncoder(EfficientNet):
         """ Calls extract_features to extract features, applies final linear layer, and returns logits. """
 
         # Convolution layers
-        features = self.extract_features(inputs)
-        connecting_layers = self._get_connecting_layers(features)
+        connecting_layers = self.extract_features(inputs)
+        connecting_layers = self._get_correct_connecting_layers(connecting_layers)
         return connecting_layers
 
-    def _get_connecting_layers(self, features):
-        idxs = {'efficientnet-b0': [0, 2, 4, 10, -1]
+    def _get_correct_connecting_layers(self, features):
+        idxs = {'efficientnet-b0': [0, 2, 4, 10, -1],
+                'efficientnet-b1': [1, 4, 7, 15, -1],
+                'efficientnet-b2': [1, 4, 7, 15, -1],
+                'efficientnet-b3': [1, 4, 7, 17, -1]
                 }[self.model_name]
         connecting_layers = [features[idx] for idx in idxs][::-1]
         return connecting_layers
 
 
-if __name__ == '__main__':
-    model = EfficientNetEncoder()
-
-    x = torch.rand((11, 3, 256, 256))
-    print([block.shape for block in model(x)])
 
 efficientnet_encoders = {
     'efficientnet-b0': {
@@ -57,5 +55,29 @@ efficientnet_encoders = {
         'pretrained_settings': {'imagenet': {}},
         'out_shapes': (320, 112, 40, 24, 16),
         'params': {'model_name': 'efficientnet-b0'}  # these are called by instantiation of EfficientNetEncoder
+    },
+    'efficientnet-b1': {
+        'encoder': EfficientNetEncoder,
+        'pretrained_settings': {'imagenet': {}},
+        'out_shapes': (320, 112, 40, 24, 16),
+        'params': {'model_name': 'efficientnet-b1'}  # these are called by instantiation of EfficientNetEncoder
+    },
+    'efficientnet-b2': {
+        'encoder': EfficientNetEncoder,
+        'pretrained_settings': {'imagenet': {}},
+        'out_shapes': (352, 120, 48, 24, 16),
+        'params': {'model_name': 'efficientnet-b1'}  # these are called by instantiation of EfficientNetEncoder
+    },
+    'efficientnet-b3': {
+        'encoder': EfficientNetEncoder,
+        'pretrained_settings': {'imagenet': {}},
+        'out_shapes': (384, 136, 48, 32, 24),
+        'params': {'model_name': 'efficientnet-b3'}  # these are called by instantiation of EfficientNetEncoder
     }
 }
+
+if __name__ == '__main__':
+    model = EfficientNetEncoder(model_name='efficientnet-b0')
+
+    x = torch.rand((11, 3, 256, 256))
+    print([block.shape for block in model(x)])
